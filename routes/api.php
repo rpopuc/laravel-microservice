10fa40981a9cfdc38d267eb32574900450448247
Robson P Oliveira <rpopuc@gmail.com>
2021-06-17 07:24:17 -0300

    feat(category): add controller

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CategoryController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => '\\'], function() {
+    Route::resource('categories', CategoryController::class, ['except' => ['create', 'edit']]);
 });
