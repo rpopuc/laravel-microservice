de3c4b035eb4507fc8f76aa03cb8482cca96ef00
Robson P Oliveira <rpopuc@gmail.com>
2021-06-17 07:42:16 -0300

    feat(category): implement store and index

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,22 +5,23 @@ namespace App\Http\Controllers\Api;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        //
+        return Category::all();
     }
 
-    public function store(Request $request)
+    public function store(CategoryRequest $request)
     {
-        //
+        return Category::create($request->all());
     }
 
     public function show(Category $category)
     {
-        //
+        return $category;
     }
 
     public function update(Request $request, Category $category)
---
+++ app/Http/Requests/CategoryRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CategoryRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:255',
+            'is_active' => 'boolean',
+        ];
+    }
+}
