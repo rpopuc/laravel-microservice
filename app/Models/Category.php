6e210deb55c4147ffbd84feeffac4b6b100304d6
Robson P Oliveira <rpopuc@gmail.com>
2021-06-17 08:28:04 -0300

    feat(category): change primary key to uuid

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
     use SoftDeletes;
+    use Uuid;
 
     protected $fillable = ['name', 'description', 'is_active'];
     protected $dates = ['deleted_at'];
+    protected $cats = ['id' => 'string'];
 }
